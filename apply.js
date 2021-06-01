9bbd0cbfc515f0c5e699194f1bfd85706df6296f
Rakhavendra <rakhavendra631999@gmail.com>
2021-06-01 21:27:22 +0530

    first commit

---
+++ apply.js
@@ -0,0 +1,11 @@
+var person = {
+    details: function(city,age){
+        return `${this.firstName} ${this.lastName} ${city} ${age}`
+    }
+}
+var person1 = {
+    firstName: "Rakhav",
+    lastName: "Pravin"
+}
+var x = person.details.apply(person1,["NLK",22]);
+console.log(x)
